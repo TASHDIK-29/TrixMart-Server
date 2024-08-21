6ab0343a1b2451c8b0152da9d08e5bf1b327d90d
Tashdik <s2110477153@ru.ac.bd>
2024-08-22 03:01:50 +0600

    authentication and home data complete

--- index.js
+++ index.js
@@ -26,26 +26,106 @@ const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+
+        const usersCollection = client.db("TrixMart").collection("users");
+        const productsCollection = client.db("TrixMart").collection("products");
+
+
+
+        app.post('/register', async (req, res) => {
+            const user = req.body;
+
+            // insert email if User does not exist
+            const query = { email: user.email };
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exist!', insertedId: null })
+            }
+
+            const salt = await bcrypt.genSalt(10)
+            const securePassword = await bcrypt.hash(req.body.password, salt)
+
+            const userInfo = {
+                // firstName: req.body.firstName,
+                // lastName: req.body.lastName,
+                // email: req.body.email,
+                // phone: req.body.phone,
+                ...user, password: securePassword,
+            }
+            const result = await usersCollection.insertOne(userInfo);
+
+            res.send(result);
+        })
+
+
+
+        app.post('/login', async (req, res) => {
+            const { email, password } = req.body;
+
+            // console.log('emailOrNumber and pin', emailOrNumber, pin);
+
+            const query = {
+                email: email
+            };
+
+            const user = await usersCollection.findOne(query);
+
+
+            if (user) {
+                const isPasswordValid = await bcrypt.compare(password, user.password);
+                if (isPasswordValid) {
+                    console.log('User exists:', user);
+
+                    const token = jwt.sign({ email: user.email }, 'SECRET_KEY', { expiresIn: '1h' });
+                    res.json({ token, user });
+
+
+                    // return res.send({ user: true, pin: true, type: user.type });
+                } else {
+                    console.log('Invalid pin');
+                    return res.send({ user: true, password: false });
+                }
+            } else {
+                console.log('User does not exist');
+                return res.send({ user: false });
+            }
+
+
+
+        })
+
+
+        // Home Data
+        app.get('/homeData', async(req, res) =>{
+            const cloths = await productsCollection.find({category : 'cloth'}).limit(5).toArray();
+            const gadgets = await productsCollection.find({category : 'gadget'}).limit(5).toArray();
+            const toys = await productsCollection.find({category : 'toy'}).limit(5).toArray();
+            const furniture = await productsCollection.find({category : 'furniture'}).limit(5).toArray();
+
+            res.send({cloths, gadgets, toys, furniture})
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
